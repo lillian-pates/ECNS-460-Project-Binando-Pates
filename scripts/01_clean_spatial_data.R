71d980b2b41a05bef166ef99b62144c1e5a5e392
lillian-pates <lillypates2022@gmail.com>
2026-04-05 14:58:21 -0600

    update

--- scripts/01_clean_spatial_data.R
+++ scripts/01_clean_spatial_data.R
@@ -5,6 +5,7 @@
 # Created: 2026-04-01
 # Last Updated: 2026-04-05
 
+# Script 1 
 # Source: Spatial Statistics for Data Science: Theory and Practice with R
 # url: https://www.paulamoraga.com/book-spatial/the-sf-package-for-spatial-vector-data.html
 # Output: Cropped Spatial Data files 
--- scripts/02_clean_flood_data.R
+++ scripts/02_clean_flood_data.R
@@ -1,5 +1,13 @@
+############################################################
+# Montana State University 
+# Project: ECNS 460 Data Analytics Spring 2026
+# Author: Bailey Binando & Lillian Pates
+# Created: 2026-04-01
+# Last Updated: 2026-04-05
 
-# Bailey Binando & Lillian Pates
+# Script 2 
+# Output: Cleaning Flood Data
+############################################################
 
 # Load Packages
 library(tidyverse)

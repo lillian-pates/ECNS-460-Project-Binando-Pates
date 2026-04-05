06189bdd0d781d27ab3a15aec61aac74db76a242
lillian-pates <lillypates2022@gmail.com>
2026-04-05 14:13:59 -0600

    Update 02_premiliary_visuals_analysis.R

--- scripts/02_premiliary_visuals_analysis.R
+++ scripts/02_premiliary_visuals_analysis.R
@@ -3,10 +3,10 @@
 # Project: ECNS 460 Data Analytics Spring 2026
 # Author: Bailey Binando & Lillian Pates
 # Created: 2026-04-01
-# Last Updated: 2026-04-
+# Last Updated: 2026-04-01
 
 # Research Question: 
-# Output: 
+# Output: Basic Visuals & Regression Analysis 
 ############################################################
 # Preliminary: Libraries 
 
@@ -18,49 +18,34 @@ library(terra)
 ############################################################
 # Preliminary: Data  
 
-## Area 
-cazones_basin = rast("data/raw/cazones_basin_dem.tif")
-entire_dem = rast("data/raw/dem_model_whole.tif")
+## Main 
+dem_pz = rast("data/processed/dem_pozarica.tif")
+rivers = st_read("data/processed/river_clipped.gdb")
 
-## Analysis
-population = rast("data/raw/mex_pop_2026_CN_100m_R2025A_v1.tiff")
-buildings = st_read("data/raw/microsoft_buildings_pozarica.gpkg")
-key_infrastructure = read_csv()
+## For Analysis
+population = st_read("data/processed/poza_rica.gpkg")
+buildings = st_read("data/processed/buildings_clipped.gpkg")
+key_infrastructure = read_csv("data/processed/poza_rica_key_infrastructure.csv")
 
-## Rivers, Water specific data 
-rivers = st_read("data/raw/HydroRIVERS_v10_na.gdb")
-flow_direction = rast("data/raw/floodplains_HAND/flow_dir.tif")
-streams = rast("data/raw/floodplains_HAND/streams.tif")
-  
 ## Flood Plains 
-flood_plain2yr = st_read("data/raw/floodplains_HAND/floodplain_HAND_2yr.shp")
-flood_plain5yr = st_read("data/raw/floodplains_HAND/floodplain_HAND_5yr.shp")
-flood_plain10yr = st_read("data/raw/floodplains_HAND/floodplain_HAND_10yr.shp")
-flood_plain25yr = st_read("data/raw/floodplains_HAND/floodplain_HAND_25yr.shp")
-flood_plain50yr = st_read("data/raw/floodplains_HAND/floodplain_HAND_50yr.shp")
-flood_plain100yr = st_read("data/raw/floodplains_HAND/floodplain_HAND_100yr.shp")
+flood_plain2yr = st_read("data/processed/floodplain_2yr.gpkg")
+flood_plain5yr =st_read("data/processed/floodplain_5yr.gpkg")
+flood_plain10yr = st_read("data/processed/floodplain_10yr.gpkg")
+flood_plain25yr = st_read("data/processed/floodplain_25yr.gpkg")
+flood_plain50yr = st_read("data/processed/floodplain_50yr.gpkg")
+flood_plain100yr = st_read("data/processed/floodplain_100yr.gpkg")
 
-floodplains_all = st_read("data/raw/floodplains_HAND/floodplains_HAND_all.geojson")
-  
-## Poza Rica Extent 
-ext_pr = ext(-98.0, -97.1, 20.2, 20.9)
-
-
-############################################################
-# Trimming to files for just Poza Rica analysis 
-
-dem_pr = crop(entire_dem, ext_pr)
 
 ############################################################
 # Deriving Slope, Aspect, Hill from DEM Model 
 
-slope = terrain(dem_pr, v = "slope", unit = "degrees")
+slope = terrain(dem_pz, v = "slope", unit = "degrees")
 
-aspect = terrain(dem_pr, v = "aspect")
+aspect = terrain(dem_pz, v = "aspect")
 
 hill = shade(
-  terrain(dem_pr, "slope"),
-  terrain(dem_pr, "aspect")
+  terrain(dem_pz, "slope"),
+  terrain(dem_pz, "aspect")
 )
 
 ############################################################

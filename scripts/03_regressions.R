c4f70315892edb641bb37ea31b9b356c56d1ac1f
lillian-pates <lillypates2022@gmail.com>
2026-04-07 22:05:00 -0600

    update

--- scripts/03_regressions.R
+++ scripts/03_regressions.R
@@ -20,7 +20,7 @@ library(terra)
 
 ## Main 
 dem_pz = rast("data/processed/dem_pozarica.tif")
-rivers = st_read("data/processed/river_clipped.gdb")
+rivers = st_read("data/processed/river_clipped.gpkg")
 flooding = read_csv("data/processed/flood_timeseries_clean.csv")
 
 
@@ -28,6 +28,8 @@ flooding = read_csv("data/processed/flood_timeseries_clean.csv")
 population = st_read("data/processed/poza_rica.gpkg")
 buildings = st_read("data/processed/buildings_clipped.gpkg")
 key_infrastructure = read_csv("data/processed/poza_rica_key_infrastructure.csv")
+expos = st_read("data/processed/building_exposure_vulnerability.gpkg")
+
 
 ## Flood Plains 
 flood_plain2yr = st_read("data/processed/floodplain_2yr.gpkg")
@@ -37,6 +39,10 @@ flood_plain25yr = st_read("data/processed/floodplain_25yr.gpkg")
 flood_plain50yr = st_read("data/processed/floodplain_50yr.gpkg")
 flood_plain100yr = st_read("data/processed/floodplain_100yr.gpkg")
 
+
+## Flood events 
+floods = read_csv("data/processed/flood_timeseries_clean.csv")
+  
 
 ############################################################
 # Deriving Slope, Aspect, Hill from DEM Model 
@@ -52,6 +58,32 @@ hill = shade(
 
 ############################################################
 # Preliminary Visuals 
+
+## Predicted river flow vs flood indications 
+floods |>
+  mutate(flow_bin = cut(pred_flow, breaks = 20)) |>
+  group_by(flow_bin) |>
+  summarise(prop_flood = mean(flood_flag),
+            mid = mean(pred_flow)) |>
+  ggplot(aes(x = mid, y = prop_flood)) +
+  geom_point() +
+  geom_smooth(se = TRUE) +
+  labs(y = "Proportion flooded", x = "Predicted flow")
+
+## Actual river flow vs flood indications
+floods |>
+  mutate(flow_bin = cut(river_flow, breaks = 20)) |>
+  group_by(flow_bin) |>
+  summarise(prop_flood = mean(flood_flag),
+            mid = mean(river_flow)) |>
+  ggplot(aes(x = mid, y = prop_flood)) +
+  geom_point() +
+  geom_smooth(se = TRUE) +
+  labs(y = "Proportion flooded", x = "Predicted flow")
+
+## Predicted River Flow vs Flood cause 
+ggplot(floods, aes(x = pred_flow , y = cause)) +
+  geom_jitter(height = 0.05, alpha = 0.3)
 
 
 
@@ -69,3 +101,19 @@ hill = shade(
 
 ############################################################
 
+
+
+protected_intersect = st_filter(population, flood_plain2yr) # default: st_intersects
+protected_within = st_filter(population, flood_plain2yr, .predicate = st_within)
+protected_intersection = st_intersection(population, flood_plain2yr)
+
+tm_shape(flood_plain2yr) + tm_polygons() +
+  tm_shape(protected_intersect) + tm_polygons(col = "green", alpha = 0.5) +
+  tm_shape(protected_intersection) + tm_polygons(col = "red", alpha = 0.5)
+
+schools_nad83 = st_transform(population, st_crs(flood_plain2yr))
+schools_tract = st_join(schools_nad83, flood_plain2yr)
+
+tmap_mode("view")
+tm_shape(flood_plain2yr) + tm_polygons(col = 'white', border.col = 'black') +
+  tm_shape(schools_tract) + tm_dots(col = 'GEOID')
